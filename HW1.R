03b54e8b91d977796c8087b3211a45082807242f
greermp <greermp@gmail.com>
2021-01-13 21:36:32 -0500

    upd

--- HW1.R
+++ HW1.R
@@ -46,9 +46,6 @@ alcoaCor <- alcoaDF[c("specbuy","speed",
 corrplot(alcoaCor, method="circle")
 
 all <- lm(satisfaction ~ speed+specbuy+pricefle+mimage+service+quality, data= alcoaDF)
-# x=ggplotRegression(speedOnSat, "#ADB9D3") 
-# ggpar(p=x, title=" Privacy v Recommend Regression", xlab = "All")
-
 summary(all)
 coef(all)
 # std coeff
@@ -67,42 +64,28 @@ confusionMatrix(factor(ifelse(predict(all, type = "response") >= .5, 1, 0)), ref
 
 
 
-speedOnSat <- lm(satisfaction ~ speed, data= alcoaDF)
-x=ggplotRegression(speedOnSat, "#ADB9D3") 
+speed <- lm(satisfaction ~ speed, data= alcoaDF)
+x=ggplotRegression(speed, "#ADB9D3") 
 ggpar(p=x, title=" Speed v satisfaction Regression")
 
-speedOnSat <- lm(satisfaction ~ specbuy, data= alcoaDF)
-x=ggplotRegression(speedOnSat, "#ADB9D3") 
+specbuy <- lm(satisfaction ~ specbuy, data= alcoaDF)
+x=ggplotRegression(specbuy, s"#ADB9D3") 
 ggpar(p=x, title="specbuy v satisfaction Regression")
 
-speedOnSat <- lm(satisfaction ~ pricefle, data= alcoaDF)
-x=ggplotRegression(speedOnSat, "#ADB9D3") 
+pricefle <- lm(satisfaction ~ pricefle, data= alcoaDF)
+x=ggplotRegression(pricefle, "#ADB9D3") 
 ggpar(p=x, title="Pricefle v satisfaction Regression")
 
-speedOnSat <- lm(satisfaction ~ mimage, data= alcoaDF)
-x=ggplotRegression(speedOnSat, "#ADB9D3") 
+mimage <- lm(satisfaction ~ mimage, data= alcoaDF)
+x=ggplotRegression(mimage, "#ADB9D3") 
 ggpar(p=x, title=" mimage v satisfaction Regression")
 
-speedOnSat <- lm(satisfaction ~ service, data= alcoaDF)
-x=ggplotRegression(speedOnSat, "#ADB9D3") 
+service <- lm(satisfaction ~ service, data= alcoaDF)
+x=ggplotRegression(service, "#ADB9D3") 
 ggpar(p=x, title=" service v satisfaction Regression")
 
-speedOnSat <- lm(satisfaction ~ quality, data= alcoaDF)
-x=ggplotRegression(speedOnSat, "#ADB9D3") 
+quality <- lm(satisfaction ~ quality, data= alcoaDF)
+x=ggplotRegression(quality, "#ADB9D3") 
 ggpar(p=x, title=" quality v satisfaction Regression")
 
 
-
-
-# ggsave('plots/FB_Priv_Recommend_Regres.jpg',  plot = p)
-fit1[7]
-summary(speedOnSat)
-coef(speedOnSat)
-lm.beta(speedOnSat)
-Anova(speedOnSat, test = "Chisq")
-exp(confint(speedOnSat))
-exp(coef(speedOnSat))
-PseudoR2(speedOnSat)
-plot(allEffects(speedOnSat))
-plot(allEffects(speedOnSat), type = "response")
-confusionMatrix(factor(ifelse(predict(speedOnSat, type = "response") >= .5, 1, 0)), reference = factor(alcoaDF$satisfaction))
