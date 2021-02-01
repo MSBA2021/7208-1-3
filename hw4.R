f978d1c9ad35b7ff934052505a53cdd64bba815a
greermp <greermp@gmail.com>
2021-01-31 21:18:59 -0500

    hw4

--- hw4.R
+++ hw4.R
@@ -19,16 +19,12 @@ package.check <- lapply(
 
 # HW Instructions
 # Alcoa wants you to examine two clustering solutions: 3 and 4 clusters of customers, with a caveat of having at least 20 customers per cluster AND where all groups appear different from each other with respect to share of wallet (usage), the validation variable.
-# 
-# Run and interpret the following just for the cluster solution you have chosen: 
-#   * ANOVA F values for IVs used to create the cluster solution chosen
-# * Which two IVs have the greatest impact on maximizing similarity within cluster and dissimilarity between clusters
-# * What are the mean differences and effect size for the validation variable via One-way ANOVA.
-# * Do this in SPSS and R..and via R give a color visualization of the cluster solution you are advocating...and why.
+
+# Make sure you set your working directory so that R knows where to look for your data (line28)
+print(getwd())
 setwd("~/MSBA/MOD3/7208 Customer Analytics/code/7208_Homework")
 
 # Read data
-
 alcoa <- read_excel('data/alcoa.xls', sheet=1)
 alcoa <- as.data.frame(alcoa)
 
