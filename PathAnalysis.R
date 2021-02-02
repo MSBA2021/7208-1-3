9a7e9af4e3431ce40e67d215a13b59c5acbebcfd
greermp <greermp@gmail.com>
2021-02-01 20:57:36 -0500

    added alcoa hw in R

--- PathAnalysis.R
+++ PathAnalysis.R
@@ -11,7 +11,38 @@ package.check <- lapply(
 )
 
 setwd("~/MSBA/MOD3/7208 Customer Analytics/code/7208_Homework")
-  
+# ======================= ALCOA===============
+dat <- read.csv("data/alcoa.csv")
+head(dat)
+nrow(dat)
+
+# It looks like you define each equation on a separate line
+model <- '
+usage  ~ satisfaction + speed 
+satisfaction ~ simage + pricefle + speed
+'
+
+
+fit <- cfa(model, data = dat)
+
+summary(fit, fit.measures = TRUE, standardized=T,rsquare=T,modindices = TRUE)
+
+
+#plot the paths a couple different ways
+semPaths(fit, 'std', whatLabels = "est", edge.label.cex = 1.1, label.cex = 2, layout = "tree2")
+
+# semPaths(fit, 'std', layout = 'tree2', intercepts = TRUE)
+# 
+# semPaths(fit, "std", "hide", label.cex=1.5)
+# 
+# 
+# semPaths(fit, "std", edge.label.cex = 0.5, curvePivot = TRUE, layout="tree2")
+
+
+
+
+#========================  WHBM Below =============
+
 dat <- read.csv("data/WHBM7040.csv")
 head(dat)
 nrow(dat)
